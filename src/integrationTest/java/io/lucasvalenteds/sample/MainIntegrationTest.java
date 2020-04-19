e7d65b7f292496851d9e63a71fab7e45d9eeb59d
Lucas <lucasvalenteds@gmail.com>
2020-04-19 01:04:43 -0300

    Add Java code

---
+++ src/integrationTest/java/io/lucasvalenteds/sample/MainIntegrationTest.java
@@ -0,0 +1,12 @@
+package io.lucasvalenteds.sample;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainIntegrationTest {
+
+    @Test
+    void testIntegrationTest() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/io/lucasvalenteds/sample/MainTest.java
@@ -0,0 +1,12 @@
+package io.lucasvalenteds.sample;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void testUnitTest() {
+        assertTrue(true);
+    }
+}
